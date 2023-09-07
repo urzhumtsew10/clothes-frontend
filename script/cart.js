ebe6b3812cfab7698a772736cc6f990b03bac608
andrew <urgumandrei@gmail.com>
2023-09-07 22:48:58 +0200

    add order form

--- script/cart.js
+++ script/cart.js
@@ -40,10 +40,23 @@ const renderProductsInCart = () => {
   calculateSumPrice();
 };
 
+const total = document.querySelector(".total__price");
 const iconCart = document.querySelector(".header__cart");
 const windowCart = document.querySelector(".windowCart");
 const contentCart = document.querySelector(".windowCart__contentCart");
 const iconCloseCart = document.querySelector(".contentCart__imgClose");
+const orderForm = document.querySelector(".windowCart__orderForm");
+const formElement = document.querySelector(".orderForm__form");
+const radioButtons = document.querySelectorAll(".payment__input");
+const formNumberCard = document.querySelector(".numberCard");
+const gratefulBlock = document.querySelector(".orderForm__gratefulBlock");
+
+const errorName = document.querySelector(".error-name");
+const errorSurname = document.querySelector(".error-surname");
+const errorEmail = document.querySelector(".error-email");
+const errorPayment = document.querySelector(".error-payment");
+const errorCard = document.querySelector(".error-card");
+const numberCardBlock = document.querySelector(".numberCard");
 
 iconCart.addEventListener("click", () => {
   document.body.style.overflow = "hidden";
@@ -110,8 +123,105 @@ windowCart.addEventListener("click", (e) => {
       renderProductsInCart();
     }
   }
+  if (e.target.classList.contains("blockInfo__btn")) {
+    if (getProductsFromStorage().length !== 0) {
+      orderForm.classList.toggle("active-order-form");
+    }
+  }
+  if (e.target.classList.contains("orderForm__cancel")) {
+    orderForm.classList.toggle("active-order-form");
+    formElement.reset();
+    errorName.classList.remove("active-error");
+    errorSurname.classList.remove("active-error");
+    errorEmail.classList.remove("active-error");
+    errorPayment.classList.remove("active-error");
+    errorCard.classList.remove("active-error");
+    formNumberCard.classList.remove("active-numberCard-form");
+  }
+  if (e.target.classList.contains("orderForm__apply")) {
+    e.preventDefault();
+    const formData = new FormData(formElement);
+    const entries = formData.entries();
+    const data = Object.fromEntries(entries);
+
+    const regEmail = new RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g);
+
+    if (data.name.trim("") === "" || !isNaN(Number(data.name))) {
+      errorName.classList.add("active-error");
+    } else {
+      errorName.classList.remove("active-error");
+    }
+
+    if (data.surname.trim("") === "" || !isNaN(Number(data.surname))) {
+      errorSurname.classList.add("active-error");
+    } else {
+      errorSurname.classList.remove("active-error");
+    }
+
+    if (!regEmail.test(data.email)) {
+      errorEmail.classList.add("active-error");
+      errorEmail.innerText = "email is invalid";
+    } else {
+      errorEmail.classList.remove("active-error");
+    }
+
+    if (data.payment === undefined) {
+      errorPayment.classList.add("active-error");
+    } else {
+      errorPayment.classList.remove("active-error");
+    }
+
+    if (numberCardBlock.classList.contains("active-numberCard-form")) {
+      if (
+        isNaN(Number(data.numberCard)) ||
+        (!isNaN(Number(data.numberCard)) && data.numberCard.length !== 16)
+      ) {
+        errorCard.classList.add("active-error");
+      } else {
+        errorCard.classList.remove("active-error");
+      }
+    }
+
+    const isValidForm =
+      !errorName.classList.contains("active-error") &&
+      !errorSurname.classList.contains("active-error") &&
+      !errorEmail.classList.contains("active-error") &&
+      !errorPayment.classList.contains("active-error") &&
+      !errorCard.classList.contains("active-error");
+
+    if (isValidForm) {
+      const orderData = {
+        clientData: data,
+        clientOrder: getProductsFromStorage(),
+        total: total.innerText,
+      };
+
+      gratefulBlock.classList.add("active-gratefulBlock");
+
+      setTimeout(() => {
+        gratefulBlock.classList.remove("active-gratefulBlock");
+        orderForm.classList.remove("active-order-form");
+        windowCart.classList.remove("active_cart");
+        contentCart.classList.remove("active-content-cart");
+        document.body.style.overflow = "inherit";
+        formElement.reset();
+      }, 5000);
+
+      localStorage.setItem("cartProducts", JSON.stringify([]));
+    }
+  }
 });
 
 if (localStorage.getItem("cartProducts") === null) {
   localStorage.setItem("cartProducts", JSON.stringify([]));
 }
+
+radioButtons.forEach((radio) => {
+  radio.addEventListener("click", (e) => {
+    if (radio.value === "card") {
+      formNumberCard.classList.add("active-numberCard-form");
+    } else {
+      formNumberCard.classList.remove("active-numberCard-form");
+    }
+  });
+});
